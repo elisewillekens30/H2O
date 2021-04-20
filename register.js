e2d936e8263ab90d4dbb5a165ebfd247b166e9d3
Elise <72791219+elisewillekens30@users.noreply.github.com>
2021-04-20 12:08:22 +0100

    Add files via upload

--- register.js
+++ register.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('https://elisewillekens30.github.io/H2O/service_worker.js');
+             .register('./service_worker.js');
   }
 }
